1bd65373b15b8413f47129f08e744fc3bc5010c2
Tej <tej@4screenmedia.com>
2022-12-26 12:15:05 +0530

    initial commit

---
+++ Biometric/src/main/java/com/example/biometric/Biometric.kt
@@ -0,0 +1,12 @@
+package com.example.biometric
+
+import android.content.Context
+import android.widget.Toast
+
+object Biometric {
+
+    fun checkBiometric(context : Context) {
+        Toast.makeText(context,"",Toast.LENGTH_LONG).show()
+    }
+
+}
